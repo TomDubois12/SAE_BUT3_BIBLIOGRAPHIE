1cd24ec53f2aaeb9eb09964b266676f10822bde8
TomDubois12 <tom.dubois@etu.univ-orleans.fr>
2024-10-23 10:13:13 +0200

    recuperation de la clé au click sur la node

--- Bokeh/src/mainPyvis.py
+++ Bokeh/src/mainPyvis.py
@@ -3,12 +3,12 @@ import numpy as np
 import pandas as pd
 import networkx as nx
 from pyvis.network import Network
-from BERT.test import search_by_keyword_and_compare, find_similar_articles, search_by_keyword
+from BERT.test import find_similar_articles, search_by_keyword
 
 def get_list_xSimilaritie(listeKey, x=5):
     """
-    Take a liste of key, for exemple 15 key similar from the keyWords and return a list of x similar article for each key.
-    return a list of list with 2 element, first: the key. second: a list of key of x article similar 
+    Take a list of key, for example 15 key similar from the keyWords and return a list of x similar articles for each key.
+    return a list of lists with 2 elements: first - the key, second - a list of keys of x similar articles.
     """
     liste_final = []
     for key in listeKey:
@@ -21,66 +21,79 @@ def show_graphique(liste_key):
     data2 = pd.read_csv(file)
     data = data2.iloc[:, :12]
 
-    # Définir la colonne "Key" comme index
+    # Define the "Key" column as the index
     data.set_index("Key", inplace=True)
 
-    # Supprimer les lignes avec des valeurs NaN dans la colonne "Publication Year"
-    # data = data.dropna(subset=["Publication Year"])
-
-    noms = data.index  # Accéder à l'index au lieu de la colonne "Key"
-    infos = data.iloc[:, 0:3]  # Prendre les infos (les colonnes restantes)
+    noms = data.index  # Access the index instead of the "Key" column
+    infos = data.iloc[:, 0:3]  # Take the info (the remaining columns)
     annees_data = data["Publication Year"]
 
-    # Rechercher par mot clé
-    # mot_cle = "Linear sweep voltammetry at very small stationary disk electrodes"
-    # res = search_by_keyword_and_compare(mot_cle)
-    # Extraire les clés de la recherche
-    # liste_cles = [(cle, cle2) for cle, cle2, valeur in res if valeur > 0.5]
-    # Reindexer le DataFrame selon les clés trouvées
     all_key1 = [t[0] for t in liste_key]
     all_key2 = [t[1] for t in liste_key]
     all_key2 = [t[i] for t in all_key2 for i in range(len(t))]
 
     dfFinal = data.reindex(all_key1 + all_key2)
 
-    noms = dfFinal.index  # Utiliser l'index (les clés)
-    infos = dfFinal.iloc[:, 0:3]  # Prendre les colonnes qui contiennent les informations
+    noms = dfFinal.index  # Use the index (the keys)
+    infos = dfFinal.iloc[:, 0:3]  # Take the columns that contain the information
 
-    # Créer un dictionnaire d'informations pour les nœuds
+    # Create a dictionary of information for the nodes
     node_info = {nom: json.dumps(dict(info), ensure_ascii=False) for nom, info in zip(noms, infos.to_dict(orient="records"))}
-    node_year = dict(zip(noms, annees_data.reindex(noms)))  # Reindexer les années pour qu'elles correspondent aux clés/noms
+    node_year = dict(zip(noms, annees_data.reindex(noms)))  # Reindex years to match the keys/names
 
-    # Créer le graphe
+    # Create the graph
     G = nx.Graph()
 
-    # Déterminer les 15 nœuds d'origine
-    origin_nodes = set(all_key1)  # Prendre les 15 clés de liste_key
+    # Determine the original nodes
+    origin_nodes = set(all_key1)  # Take the 15 keys from liste_key
 
-    # Ajouter les nœuds avec les attributs 'infos' et 'year', en définissant la couleur
+    # Add the nodes with attributes 'infos' and 'year', defining the color
     for nom in noms:
-        color = 'red' if nom in origin_nodes else 'blue'  # Rouge pour les nœuds d'origine, bleu sinon
+        color = 'red' if nom in origin_nodes else 'blue'  # Red for origin nodes, blue otherwise
         G.add_node(nom, infos=node_info[nom], year=node_year[nom], color=color)
 
-    # Grouper par année et ajouter les arêtes
-    grouped_by_annee = dfFinal.groupby("Publication Year").apply(lambda x: list(x.index))  # Utiliser l'index ici
-    # print(grouped_by_annee)
+    # Add the edges
     for key, keys in liste_key:
         list_tuple_cles = [(key, t) for t in keys]
         G.add_edges_from(list_tuple_cles, color="000000")
 
-    # for group in grouped_by_annee:
-    #    G.add_edges_from((group[i], group[j]) for i in range(len(group)) for j in range(i + 1, len(group)))
-
-    # Visualiser avec PyVis
+    # Visualize with PyVis
     nt = Network('100vh', '100vw', notebook=True)
     nt.show_buttons(filter_=['physics'])
     nt.from_nx(G)
 
-    # Définir la couleur des nœuds dans Pyvis
+    # Set the color of the nodes in Pyvis
     for node in G.nodes(data=True):
         nt.get_node(node[0])['color'] = node[1]['color']
 
-    nt.show('Bokeh/bin/nx.html')
+    # Create the HTML file
+    html_file_path = 'Bokeh/bin/nx.html'
+    nt.save_graph(html_file_path)
+
+    # Manually modify the HTML to include the JavaScript functionality
+    with open(html_file_path, 'r') as f:
+        html_content = f.read()
+
+    # Add custom script for handling node clicks
+    custom_script = """
+    <script type="text/javascript" src="Bokeh/bin/lib/binding/utils.js"></script>
+    <script type="text/javascript">
+        function onNodeClick(params) {
+            if (params.nodes.length > 0) {
+                console.log("Clicked node:", params.nodes[0]);
+                // Here you can add more functionality, like fetching data
+            }
+        }
+        network.on("click", onNodeClick);
+    </script>
+    """
+    
+    # Insert the custom script just before the closing </body> tag
+    html_content = html_content.replace('</body>', custom_script + '</body>')
+
+    # Write the modified content back to the file
+    with open(html_file_path, 'w') as f:
+        f.write(html_content)
 
 print("-" * 50)
 mot_cle = "Linear sweep voltammetry at very small stationary disk electrodes"
