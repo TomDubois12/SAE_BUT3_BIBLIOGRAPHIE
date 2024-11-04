1fe2df36afc28237bc5fe44e5e32b3901d782753
boissaynathan <nathan.boissay@etu.univ-orleans.fr>
2024-11-04 16:35:38 +0100

    Ajout d'un color picker

--- AppElectronJS/preload.js
+++ AppElectronJS/preload.js
@@ -1,7 +1,15 @@
+const fs = require("fs");
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('api', {
+
+contextBridge.exposeInMainWorld('api', { 
     callFunctionSearch: async (query) => {
-        return await ipcRenderer.invoke('callFunctionSearch', query);  // Notez 'callFunctionSearch'
-    }
+        return await ipcRenderer.invoke('callFunctionSearch', query);
+    },
+    
+    readFile: (path, callback) => fs.readFile(path, 'utf-8', callback),
+    writeFile: (path, data, callback) => fs.writeFile(path, data, callback),
 });
+
+
+  
